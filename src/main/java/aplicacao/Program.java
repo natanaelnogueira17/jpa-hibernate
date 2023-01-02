e19f99e01a1df687ba63327bf4e8c95867fb3357
Natanael Nogueira <102445138+natanaelnogueira17@users.noreply.github.com>
2023-01-02 17:06:16 -0300

    prendendo o JPA com HIBERNATE - config

---
+++ src/main/java/aplicacao/Program.java
@@ -0,0 +1,19 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		Pessoa p = em.find(Pessoa.class, 1);
+		System.out.println(p);
+		System.out.println("pronto");
+		
+		}
+}
